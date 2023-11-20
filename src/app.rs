f48f0c96feeadaec7181d2cc454895bedad6d859
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2023-11-19 20:07:42 -0500

    layout, styles, theme from fibermc

--- src/app.rs
+++ src/app.rs
@@ -12,42 +12,82 @@ pub fn App(cx: Scope) -> impl IntoView {
     view! {
         cx,
 
-        // injects a stylesheet into the document <head>
-        // id=leptos means cargo-leptos will hot-reload this stylesheet
-        <Stylesheet id="leptos" href="/pkg/leptos_start.css"/>
+            <Style media="screen" id="palette_light_dark" />
+            <Style>
+                """
+                body#page_container {
+                    width: 100vw;
+                    height: 100vh;
+                }
+                main#content_main {
+                    overflow: auto;
+                }
+                """
+            </Style>
+            <Script src="https://static.jpcode.dev/js/multi-palette.min.js" />
 
-        // sets the document title
-        <Title text="Welcome to Leptos"/>
+            // injects a stylesheet into the document <head>
+            // id=leptos means cargo-leptos will hot-reload this stylesheet
+            <Stylesheet id="leptos" href="/pkg/leptos_start.css"/>
+            <Stylesheet href="https://www.fibermc.com/css/core_style.css?v=1.0.1"/>
+            // <Stylesheet href="https://www.fibermc.com/css/search.css?v=1.0.1"/>
+            <Stylesheet href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
 
-        // content for this welcome page
-        <Router>
-            <main>
-                <ErrorBoundary
-                    // the fallback receives a signal containing current errors
-                    fallback=|cx, errors| view! { cx,
-                        <div class="error">
-                            <p>"An unexpected error occurred! Errors: "</p>
-                            // we can render a list of errors as strings, if we'd like
-                            <ul>
-                                {move || errors.get()
-                                    .into_iter()
-                                    .map(|(_, e)| view! { cx, <li>{e.to_string()}</li>})
-                                    .collect_view(cx)
-                                }
-                            </ul>
-                        </div>
-                    }
-                >
-                    <Routes>
-                        <Route path="" view=|cx| view! { cx, <HomePage/> }/>
-                        <Route path="stats/:mod_id" view=|cx| {
-                            let params = use_params::<StatsPageParams>(cx);
-                            view! { cx, <StatsPage params=params/> }
-                        }/>
-                    </Routes>
-                </ErrorBoundary>
-         </main>
-        </Router>
+
+            // sets the document title
+            <Title text="Welcome to Leptos"/>
+
+        <body id="page_container">
+
+            <header>
+                <div id="navbar">
+                    <div id="navbar_front" class="flex row">
+                        // <!-- <button id="menu_button" class="h_button icon_only"><i class=material-icons>menu</i></button> -->
+                        <a href="https://www.fibermc.com"><h1 id="title" class="header_element">"FiberMC"</h1></a>
+                        <h2 id="subtitle" class="header_element">"Minecraft Fabric Mod List"</h2>
+                    </div>
+                    <div class="end flex row">
+                        <a href="https://ko-fi.com/johnpaul" class="logo button" target="_blank" rel="noopener noreferrer"><img src="https://static.jpcode.dev/img/icon/ko-fi.svg" alt="ko-fi" class="invert icon_after"/></a>
+                        <a href="https://patreon.com/jpcode" class="logo button" target="_blank" rel="noopener noreferrer"><img src="https://static.jpcode.dev/img/icon/patreon.svg" alt="Patreon" class="invert icon_after"/></a>
+                        <a href="https://github.com/John-Paul-R/fibermc" class="logo button" target="_blank" rel="noopener noreferrer"><img src="https://static.jpcode.dev/img/icon/github.svg" alt="GitHub" class="invert icon_after"/></a>
+                        <a href="https://discord.jpcode.dev" class="logo button" target="_blank" rel="noopener noreferrer"><img src="https://static.jpcode.dev/img/icon/discord.svg" alt="Discord" class="invert icon_after"/></a>
+                        <a id="about" href="./about" class="button"><span class="text">"About"</span></a>
+                        <button class="swap_palette"><span class="text">"Theme"</span><i class="material-icons">"style"</i></button>
+                    </div>
+                </div>
+            </header>
+
+            <Router>
+                <main id="content_main">
+                    <div id="content_body">
+                        <ErrorBoundary
+                            // the fallback receives a signal containing current errors
+                            fallback=|cx, errors| view! { cx,
+                                <div class="error">
+                                    <p>"An unexpected error occurred! Errors: "</p>
+                                    // we can render a list of errors as strings, if we'd like
+                                    <ul>
+                                        {move || errors.get()
+                                            .into_iter()
+                                            .map(|(_, e)| view! { cx, <li>{e.to_string()}</li>})
+                                            .collect_view(cx)
+                                        }
+                                    </ul>
+                                </div>
+                            }
+                        >
+                            <Routes>
+                                <Route path="" view=|cx| view! { cx, <HomePage/> }/>
+                                <Route path="stats/:mod_id" view=|cx| {
+                                    let params = use_params::<StatsPageParams>(cx);
+                                    view! { cx, <StatsPage params=params/> }
+                                }/>
+                            </Routes>
+                        </ErrorBoundary>
+                    </div>
+                </main>
+            </Router>
+        </body>
     }
 }
 
